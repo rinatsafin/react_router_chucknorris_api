2a4d05062e3ced5a040a42c0fd81e4fd1f67bb23
rinatsafin <safinrinat87@gmail.com>
2018-08-30 23:34:42 +0300

    next step

--- dev/components/Header/index.js
+++ dev/components/Header/index.js
@@ -12,7 +12,6 @@ class Header extends React.Component {
   constructor() {
     super();
     this.state = {
-      loading: false,
       categories: [],
     }
   }
@@ -22,10 +21,6 @@ class Header extends React.Component {
   }
 
   getCategory() {
-    this.setState({
-      loading: true,
-    });
-
     fetch(API_GET_CATEGORIES_URL)
       .then(response => response.json())
       .then((response) => {
@@ -48,18 +43,13 @@ class Header extends React.Component {
         <h1>Get Jokes with chucknorris.io</h1>
         <nav>
           <ul className="menu">
-            { categories.length ? 
-              categories.map(cat => {return (<h1 key={cat.length}>{cat}</h1>)}) : "empty"
-            }
-            <li>
-              <NavLink to="/news" activeClassName="active">news</NavLink>
-            </li>
-            <li>
-              <NavLink to="/news/10" activeClassName="active">news</NavLink>
-            </li>
-            <li>
-              <NavLink to="/topics" activeClassName="active">topics</NavLink>
-            </li>
+            { categories.length ? (
+              categories.map(cat => (<li key={Date.now()}><NavLink to={`/${cat}`} activeClassName="active">{cat}</NavLink></li>))
+            ) : (
+              <li>
+                <NavLink to="/news" activeClassName="active">news</NavLink>
+              </li>
+            )}
           </ul>
         </nav>
       </header>
